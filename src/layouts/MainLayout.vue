325590d868074032a92b1fafbc5d09cd84cd4ee3
MichaelBruwer <michaelbruwer21@gmail.com>
2021-04-22 15:54:31 +0200

    Initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,53 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class="bg-white text-grey-10" bordered>
+      <q-toolbar class="constrain">
+        <q-btn to="/camera" class="large-screen-only q-mr-sm" flat round icon="eva-camera-outline" size="18px" dense/>
+        <q-separator class="large-screen-only" vertical spaced="true"/>
+        <q-toolbar-title class="text-bold">
+          Quasagram
+        </q-toolbar-title>
+         <q-btn class="large-screen-only" to="/" flat round icon="eva-home-outline" size="18px" dense/>        
+      </q-toolbar>
+    </q-header>
+
+    <q-footer class="bg-white small-screen-only" bordered>     
+        <q-tabs class="text-grey-10" active-color="primary" indicator-color="transparent">
+        <q-route-tab to="/" name="Home" icon="eva-home-outline" />
+        <q-route-tab to="/camera" name="Camera" icon="eva-camera-outline" />
+        
+      </q-tabs>
+    </q-footer>
+          
+
+    <q-page-container class="bg-grey-1">
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+export default {
+  name: 'MainLayout',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+.q-toolbar
+  @media (min-width: $breakpoint-sm-min)
+    height: 77px
+.q-toolbar__title 
+  font-size: 30px
+  @media (max-width: $breakpoint-xs-max)
+    text-align: center
+
+.q-footer 
+  .q-tab__icon
+    font-size: 30px
+</style>
